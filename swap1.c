f5d329a5a5862ae87f65c58e8fcc997cd876e256
rishav-0317 <rishavpandey7255@gmail.com>
2026-05-12 13:47:06 +0530

    newm

---
+++ swap1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,temp;
+    printf("Enter the value of a and b: ");
+    scanf("%d %d",&a,&b);
+    temp = a;
+    a=b;
+    b=temp;
+    printf("After swapping: a = %d, b = %d",a,b);
+    return 0;
+}
