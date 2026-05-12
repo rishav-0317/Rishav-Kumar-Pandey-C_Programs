424431f8e5e424eabcf8b00912dceb7b4fb6cc21
rishav-0317 <rishavpandey7255@gmail.com>
2026-05-12 13:41:31 +0530

    newwm

---
+++ datatypes.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main(){
+    int a;
+    float b;
+    char c;
+    double d;
+    long int e;
+    long double f;
+    printf("Size of int: %lu bytes\n", sizeof(a));
+    printf("Size of float: %lu bytes\n", sizeof(b));
+    printf("Size of char: %lu bytes\n", sizeof(c));
+    printf("Size of double: %lu bytes\n", sizeof(d));
+    printf("Size of long int: %lu bytes\n", sizeof(e));
+    printf("Size of long double: %lu bytes\n", sizeof(f));
+
+}
+   
